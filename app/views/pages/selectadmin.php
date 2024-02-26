a52a6dd91f33ddddee9310250341d74bd4176dbe
Liviru Samarawickrama <120428964+liviru-sama@users.noreply.github.com>
2024-02-27 05:21:41 +0530

    update ccm/login dashboard error

--- app/views/pages/selectadmin.php
+++ app/views/pages/selectadmin.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SELECT ADMIN </title>
+    <title>Admin Login </title>
     <link rel="stylesheet" type="text/css" href="<?php echo CSS;?>admin/selectadmin.css">
     
 </head>
